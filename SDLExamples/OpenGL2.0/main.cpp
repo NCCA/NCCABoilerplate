5387efc2c4657ff9afa928b316614ed4da7fb527
jmacey <jmacey@bournemouth.ac.uk>
2023-06-29 10:54:52 +0100

    updated README

---
+++ SDLExamples/OpenGL2.0/main.cpp
@@ -0,0 +1,182 @@
+#ifdef WIN32
+#define SDL_MAIN_HANDLED
+#include <windows.h>
+#endif
+
+#include <SDL2/SDL.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <cstdlib>
+#include <string>
+#include <iostream>
+
+/// this c an be compiled on a mac using g++ -Wall -g main.cpp  -o GL  `sdl2-config --cflags --libs` -D DARWIN -framework OpenGL
+///
+
+#if defined(__linux__) || defined(_WIN32)
+#include <GL/gl.h>
+#endif
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#endif
+
+/// @brief function to quit SDL with error message
+/// @param[in] _msg the error message to send
+void SDLErrorExit(const std::string &_msg);
+
+/// @brief initialize SDL OpenGL context
+SDL_GLContext createOpenGLContext(SDL_Window *window);
+
+void drawTriangle();
+
+int main()
+{
+
+  // Initialize SDL's Video subsystem
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK | SDL_INIT_HAPTIC) < 0)
+  {
+    // Or die on error
+    SDLErrorExit("Unable to initialize SDL");
+  }
+
+  // now get the size of the display and create a window we need to init the video
+  SDL_Rect rect;
+  SDL_GetDisplayBounds(0, &rect);
+  // now create our window
+  SDL_Window *window = SDL_CreateWindow("SDL 2 and compat OpenGL",
+                                        SDL_WINDOWPOS_CENTERED,
+                                        SDL_WINDOWPOS_CENTERED,
+                                        rect.w / 2,
+                                        rect.h / 2,
+                                        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+  // check to see if that worked or exit
+  if (!window)
+  {
+    SDLErrorExit("Unable to create window");
+  }
+
+  // Create our OpenGL context and attach it to our window
+
+  SDL_GLContext glContext = createOpenGLContext(window);
+  if (!glContext)
+  {
+    SDLErrorExit("Problem creating OpenGL context");
+  }
+  // make this our current GL context (we can have more than one window but in this case not)
+  SDL_GL_MakeCurrent(window, glContext);
+  /* This makes our buffer swap syncronized with the monitor's vertical refresh */
+  SDL_GL_SetSwapInterval(1);
+  // now setup a basic camera for viewing
+  glEnable(GL_DEPTH_TEST);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH);
+  glMatrixMode(GL_PROJECTION);
+  // set the projection matrix to prespective from glm
+  float aspect = static_cast<float>(rect.w) / rect.h;
+  auto persp = glm::perspective(45.0f, aspect, 0.5f, 100.0f);
+  glLoadIdentity();
+  glMultMatrixf(&persp[0][0]);
+  // now create a static camera matrix for viewing the object
+  glMatrixMode(GL_MODELVIEW);
+  auto look = glm::lookAt(glm::vec3(2, 2, 2), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+  glLoadIdentity();
+  glMultMatrixf(&look[0][0]);
+
+  bool quit = false;
+  // sdl event processing data structure
+  SDL_Event event;
+  while (!quit)
+  {
+
+    while (SDL_PollEvent(&event))
+    {
+      switch (event.type)
+      {
+      // this is the window x being clicked.
+      case SDL_QUIT:
+        quit = true;
+        break;
+      // if the window is re-sized pass it to the ngl class to change gl viewport
+      // note this is slow as the context is re-create by SDL each time
+      case SDL_WINDOWEVENT:
+
+        break;
+
+      // now we look for a keydown event
+      case SDL_KEYDOWN:
+      {
+        switch (event.key.keysym.sym)
+        {
+        // if it's the escape key quit
+        case SDLK_ESCAPE:
+          quit = true;
+          break;
+        case SDLK_w:
+          glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+          break;
+        case SDLK_s:
+          glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+          break;
+        case SDLK_f:
+          SDL_SetWindowFullscreen(window, SDL_TRUE);
+          break;
+
+        case SDLK_g:
+          SDL_SetWindowFullscreen(window, SDL_FALSE);
+          break;
+        default:
+          break;
+        } // end of key process
+      }   // end of keydown
+
+      } // end of event switch
+    }   // end of poll events
+
+    // now clear screen and render
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    int w, h;
+    SDL_GetWindowSize(window, &w, &h);
+    glViewport(0, 0, w, h);
+    // draw a triangle
+    drawTriangle();
+    // swap the buffers
+    SDL_GL_SwapWindow(window);
+  }
+  // now tidy up and exit SDL
+  SDL_Quit();
+}
+
+SDL_GLContext createOpenGLContext(SDL_Window *window)
+{
+  // Request an opengl 2.1 context first we setup our attributes, if you need any
+  // more just add them here before the call to create the context
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+  return SDL_GL_CreateContext(window);
+}
+
+void SDLErrorExit(const std::string &_msg)
+{
+  std::cerr << _msg << "\n";
+  std::cerr << SDL_GetError() << "\n";
+  SDL_Quit();
+  exit(EXIT_FAILURE);
+}
+
+void drawTriangle()
+{
+  static float rot = 0.0f;
+  glPushMatrix();
+  glRotatef(rot, 0, 1, 0);
+  glBegin(GL_TRIANGLES);
+  glColor3f(1, 0, 0);
+  glVertex3d(0, 1, 0);
+  glColor3f(0, 1, 0);
+  glVertex3d(1, -1, 0);
+  glColor3f(0, 0, 1);
+  glVertex3d(-1, -1, 0);
+  glEnd();
+  glPopMatrix();
+  rot++;
+}
--- SDLExamples/README.md
+++ SDLExamples/README.md
@@ -3,3 +3,4 @@
 This folder contains a number of SDL2 demos, each demo is in its own subdirectory.
 
 -[PutPixel](./PutPixel/) - This is a simple demo that shows how to draw a pixel to the screen using the SDL2 library.
+-[OpenGL](./OpenGL/) - This is a simple demo that shows how to use OpenGL with SDL2. It uses the glm library to setup projection and camera matrices but uses the deprecated OpenGL fixed function pipeline to draw a triangle to the screen.
